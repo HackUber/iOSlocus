ffaaaf450f8f31347ebd2bee0a26c3c68cabb3fc
Marcelo Cristiano <marceloabk@hotmail.com>
2018-05-06 04:33:13 -0300

    Fixing projext

---
+++ Controller/GroupViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GroupViewController.swift
+//  Locus
+//
+//  Created by Ludimila da Bela Cruz on 06/05/18.
+//  Copyright © 2018 Locus. All rights reserved.
+//
+
+import UIKit
+
+class GroupViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "groups", for: indexPath) as! GroupTableViewCell
+        
+        cell.imageGroup.image = #imageLiteral(resourceName: "car")
+        cell.nameGroup.text = "Sobradinho"
+        return cell
+    }
+    
+}
